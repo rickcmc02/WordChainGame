daf7ff54a35acb595bfbd6602633ab61649e7b34
melody <thgilnoomelody@gmail.com>
2020-07-14 10:19:33 +0900

    New start

--- src/wcg/Main.java
+++ src/wcg/Main.java
@@ -3,8 +3,7 @@ package wcg;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("±è¿ë¼ö : ´Ü¾î");
-		System.out.println("¾çÈñ¿µ : ¾îºÎ");
+		System.out.println("ì´ì¬í˜• : ì†Œë…ì œ");
 	}
 
 }
